04a07eb01615667d46bcbabfd3b4146a4727d3c1
Daniel Dorado <danieldorado27@gmail.com>
2022-11-14 17:11:09 -0500

    [CREATE] First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const app = express()
+const router = require('./router')
+
+module.exports = (database = {}, utils = {}) => {
+  app.use(express.json())
+  app.use(express.urlencoded())
+  app.use(fileUpload())
+  app.use(cors({ origin: '*' }))
+  router(app, database, utils)
+
+  return app
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const makeApp = require('./app')
+const database = require('./database/index')
+const utils = require('./utils/index')
+
+const port = process.env.PORT || 3000
+const app = makeApp(database, utils)
+
+app.listen(port, () => {
+  console.log(`Server is running - Port: ${port} - Start time: ${Date(Date.now()).toString()}`)
+})
