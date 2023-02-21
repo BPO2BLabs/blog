5f6eb9f80cf6578e821c96b471e50530bc59b481
Daniel Dorado <danieldorado27@gmail.com>
2023-02-21 15:10:58 -0500

    [UPDATE] admin endpoint

--- components/index.js
+++ components/index.js
@@ -3,6 +3,7 @@ const comments = require('./comments/index')
 const files = require('./files/index')
 const auth = require('./auth/index')
 const consult = require('./consult/index')
+const admin = require('./admin/index')
 
 module.exports = (app, database, utils) => {
   app.use('/posts', posts(database, utils))
@@ -10,4 +11,5 @@ module.exports = (app, database, utils) => {
   app.use('/files', files(database, utils))
   app.use('/auth', auth(database, utils))
   app.use('/consult', consult(database, utils))
+  app.use('/admin', admin(database, utils))
 }
